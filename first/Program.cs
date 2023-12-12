1d094196933ac2806b655ec0b77e7f2592231fc7
Elionora01 <elinabakirova15749@gmail.com>
2023-12-12 17:31:17 +0500

    first commit

---
+++ first/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+ int product = 1;// для заполнения от 1 до ...
+            int[,] arr = new int[5, 4];// размер 5 на 4; строк 5 столбцов 4
+            // заполним массив и выведем
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = product++;
+                    Console.Write($"{arr[i, j],3}");// выводим массив
+                }
+                Console.WriteLine();
+            }
+            // начинаем искать
+            Console.Write("Какую строку будем искать ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Какой столбец будем искать ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            // проверим условием границы 
+            if (l <= arr.GetLength(0) - 1 && c <= arr.GetLength(1) - 1)
+                Console.WriteLine("Элемент массива " + arr[l, c]);
+            else
+                Console.WriteLine("Такого элемента нет");
+                
---
+++ second/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+
+
+  int[,] matrix = new int[,]
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int numRows = matrix.GetLength(0); // Получаем количество строк в матрице
+        int numCols = matrix.GetLength(1); // Получаем количество столбцов в матрице
+
+        for (int i = 0; i < numCols; i++) // Меняем местами первую и последнюю строки
+        {
+            int temp = matrix[0, i];
+            matrix[0, i] = matrix[numRows - 1, i];
+            matrix[numRows - 1, i] = temp;
+        }
+
+        for (int i = 0; i < numRows; i++) // Вывод перевёрнутой матрицы
+        {
+            for (int j = 0; j < numCols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+            
